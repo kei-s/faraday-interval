110cb1da52ebca8b5caf3be32c7fdbd51a3d2958
kei-s <kei.shiratsuchi@gmail.com>
2016-10-17 19:51:18 +0900

    Implement interval

--- lib/faraday/interval.rb
+++ lib/faraday/interval.rb
@@ -1,7 +1,23 @@
-require "faraday/interval/version"
+require 'faraday'
+require 'faraday/interval/version'
 
 module Faraday
   module Interval
-    # Your code goes here...
+    class Middleware < Faraday::Middleware
+      def initialize(app, options = {})
+        super(app)
+        @second = options[:second] || 1
+        @last_call = nil
+      end
+
+      def call(env)
+        sleep 1 while @last_call && Time.now < @last_call + @second
+        @app.call(env).on_complete do
+          @last_call = Time.now
+        end
+      end
+    end
   end
+
+  Request.register_middleware interval: lambda { Interval::Middleware }
 end
